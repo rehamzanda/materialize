81ef5c93413827b9b35237ed576b36be21994153
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-26 17:37:57 +0200

    fixed js

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const displayMsg = document.querySelector("#display-msg");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  displayMsg.textContent = `Thank you for chose us!`;
-  console.log(displayMsg);
-  displayMsg.className = " green darken-2 flow-text";
+  displayMsg.textContent = `Thank you for choosing us!`;
+  displayMsg.className = "green darken-2 flow-text";
+  displayMsg.style.display = "block"; // Ensure the element is visible
 });
 
-
+// Set 3 seconds for the message to disappear from the screen:
+setTimeout(() => {
+  displayMsg.style.display = "none"; // Hide the element after 3 seconds
+}, 3000);
