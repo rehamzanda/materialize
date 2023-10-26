95082b92d186414e268d588232a7d6331d7aaf66
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-26 16:59:16 +0200

    remove msg from screen after 3 sec

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ submitBtn.addEventListener("click", (e) => {
   displayMsg.textContent = `Thank you for chose us!`;
   console.log(displayMsg);
   displayMsg.className = " green darken-2 flow-text";
+
+  //set 3 seconds to message dissapear from screen:
+  setTimeout(() => {
+    displayMsg.style.display = "none";
+  }, 3000);
 });
-
-
