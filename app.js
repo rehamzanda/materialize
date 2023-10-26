49cfaf28e4b6b413d61936a7a3798ef8f7b4a4fa
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-26 17:25:19 +0200

    fixed message by javascript by click submit button

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ submitBtn.addEventListener("click", (e) => {
   displayMsg.textContent = `Thank you for chose us!`;
   console.log(displayMsg);
   displayMsg.className = " green darken-2 flow-text";
+  displayMsg.style.display = 'block';
 });
 
-
+//set 3 seconds to message dissapear from screen:
+setTimeout(() => {
+  displayMsg.style.display = "none";
+}, 3000);
